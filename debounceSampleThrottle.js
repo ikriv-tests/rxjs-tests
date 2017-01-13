b22818d27410904d505371f3ebc7c4d7ee37cd35
ivan.krivyakov <ivan.krivyakov@thomsonreuters.com>
2017-01-12 23:28:02 -0500

    Added debounce-sample-throttle comparison

---
+++ debounceSampleThrottle.js
@@ -0,0 +1,29 @@
+let {Observable} = require("rx")
+
+Observable.prototype.relativeTimeStamp = function() {
+	let start = Date.now();
+	return this.map( v => ({ value: v, timeStamp: Date.now()-start }) );
+}
+
+function burst(n) {
+	let items = ['a','b','c','d','e','f','g','h'];
+	return Observable.from(items.map(x=>(n+1)+":"+x));
+}
+
+function printValue({timeStamp,value}) {
+	console.log(timeStamp, value);
+}
+
+function noOp() {}
+
+let periodicBurst = Observable.timer(0,500).switchMap(burst).take(20); // 3 bursts, last incomplete
+
+console.log("ORIGINAL SEQUENCE");
+periodicBurst.relativeTimeStamp().subscribe(printValue, noOp, ()=> {
+	console.log("DEBOUNCE");
+	periodicBurst.relativeTimeStamp().debounce(750).subscribe(printValue, noOp, ()=> {
+		console.log("SAMPLE");
+		periodicBurst.relativeTimeStamp().sample(750).subscribe(printValue, noOp, ()=> {
+			console.log("THROTTLE");
+			periodicBurst.relativeTimeStamp().throttle(750).subscribe(printValue)})})});
+			
